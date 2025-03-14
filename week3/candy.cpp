c119e7603d291cc512069b4081f3d08c5eb495e5
py <piggy56425@gmail.com>
2025-03-14 23:06:45 +0800

    png

--- week3/candy.cpp
+++ week3/candy.cpp
@@ -1,30 +1,23 @@
 class Solution {
     public:
         int candy(vector<int>& ratings) {
-            int n = ratings.size();
-            
-            //give each child one candy at first time
-            vector<int> candies(n, 1);
-            
-            bool stop = false;
-            while(!stop){
-                stop = true;
-                for(int i = 0; i < n; ++i){
-                    if(i > 0 && ratings[i] > ratings[i-1]){
-                        if(candies[i] <= candies[i-1]){
-                            stop = false;
-                            candies[i] = candies[i-1]+1;
-                        }
-                    }
-                    if(i < n-1 && ratings[i] > ratings[i+1]){
-                        if(candies[i] <= candies[i+1]){
-                            stop = false;
-                            candies[i] = candies[i+1]+1;
-                        }
-                    }
-                }   
+            vector<int> candy_list(ratings.size(), 1);
+     
+            for (int i = 0; i < ratings.size() - 1; i++)
+            {
+                if (ratings[i] < ratings[i + 1])
+                {
+                    candy_list[i + 1] = candy_list[i] + 1;
+                }
             }
-            
-            return accumulate(candies.begin(), candies.end(), 0);
+     
+            for (int i = ratings.size() - 1; i >= 1; i--)
+            {
+                if (ratings[i - 1] > ratings[i] && candy_list[i-1] <= candy_list[i])  
+                {
+                    candy_list[i - 1] = candy_list[i] + 1;
+                }
+            }
+            return accumulate(candy_list.begin(), candy_list.end(), 0);
         }
     };
